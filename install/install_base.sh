49d5371257b3ef79522231073139be903b8ea3ad
Vivi <vietpiano@gmail.com>
2020-03-05 00:43:50 +0700

    1

--- install/install_base.sh
+++ install/install_base.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export PATH=$PATH:/sbin
 
-#Install git
+#Install git + phalcon
 yum install git -y
-yum update -y
-yum install yum-utils -y
+yum install epel-release yum-utils -y
 yum-config-manager --enable remi-php73
 yum install php-devel -y
 git clone https://github.com/phalcon/cphalcon.git -b 3.4.x /tmp/cphalcon
@@ -15,4 +14,15 @@ service php-fpm restart
 service nginx restart
 
 cd /tmp
+git clone https://github.com/kidx5452/vesta.git vesta_tpl
+cp /tmp/vesta_tpl/templates/* /usr/local/vesta/data/templates/web/nginx/php-fpm
 
+#Install nodejs
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+yum install nodejs -y
+
+#Install redis
+yum install redis -y
+yum install php-redis -y
+service php-fpm restart
+service nginx restart
