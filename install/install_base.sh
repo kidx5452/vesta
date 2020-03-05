17b57f33d95a396e88e0793fce0b783862992319
Vivi <vietpiano@gmail.com>
2020-03-05 18:29:26 +0700

    1

--- install/install_base.sh
+++ install/install_base.sh
@@ -10,11 +10,9 @@ git clone https://github.com/phalcon/cphalcon.git -b 3.4.x /tmp/cphalcon
 cd /tmp/cphalcon/build/
 ./install >> log
 echo "extension=phalcon.so" > /etc/php.d/phalcon.ini
-service php-fpm restart
-service nginx restart
 
-cd /tmp
-git clone https://github.com/kidx5452/vesta.git vesta_tpl
+
+git clone https://github.com/kidx5452/vesta.git /tmp/vesta_tpl
 cp /tmp/vesta_tpl/templates/* /usr/local/vesta/data/templates/web/nginx/php-fpm
 
 #Install nodejs
@@ -22,7 +20,9 @@ curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 yum install nodejs -y
 
 #Install redis
-yum install redis -y
-yum install php-redis -y
+yum install redis php-redis -y
+service redis start
+systemctl enable redis
 service php-fpm restart
 service nginx restart
+
