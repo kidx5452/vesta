1c03a25745af518b346f43b5d7836ebb912bf3a9
Vivi <vietpiano@gmail.com>
2020-03-08 02:55:44 +0700

    1

--- install/install_base.sh
+++ install/install_base.sh
@@ -6,7 +6,7 @@ yum install git -y
 yum install epel-release yum-utils -y
 yum-config-manager --enable remi-php73
 yum install php-devel -y
-yum --enablerepo=remi-php73 install -y php-phalcon3
+yum --enablerepo=remi-php73 install -y php-phalcon3 php-redis
 #git clone https://github.com/phalcon/cphalcon.git -b 3.4.x /tmp/cphalcon
 #cd /tmp/cphalcon/build/
 #./install >> log
@@ -19,9 +19,11 @@ git clone https://github.com/kidx5452/vesta.git /tmp/vesta_tpl
 #Install nodejs
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 yum install nodejs -y
+#Install pm2
+npm install -g pm2
 
 #Install redis
-yum install redis php-redis -y
+yum install redis -y
 service redis start
 systemctl enable redis
 service php-fpm restart
